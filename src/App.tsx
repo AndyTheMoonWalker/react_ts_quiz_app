4d7d2f044e06c770477cfaff1448ff1046b9b0f9
Andy <none>
2022-08-11 19:05:54 +0200

    card component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
 import './App.css';
+import QuestionCard from './components/question-card.component';
 
 function App() {
-	return <div className='App'>Test</div>;
+	const start = async () => {};
+	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+	const nextQuestion = () => {};
+	return (
+		<div className='App'>
+			<h1>Quiz Game</h1>
+			<button className='button_start' onClick={start}>
+				Start!
+			</button>
+			<p>Score</p>
+			<p>Loading ...</p>
+			<QuestionCard />
+			<button className='button_next' onClick={nextQuestion}>
+				Next!
+			</button>
+		</div>
+	);
 }
 
 export default App;
